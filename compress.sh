c655908c83672c0bf37e0f03ef06060bd59cb565
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2022-05-31 00:21:43 -0400

    personalized compress.sh

--- compress.sh
+++ compress.sh
@@ -2,82 +2,87 @@
 #
 # Re-encode a video to a target size in MB.
 # Example:
-#    ./this_script.sh video.mp4 15
-# FROM https://stackoverflow.com/a/61146975/5666845
+#    ./compress.sh video.mp4 15
+# based on https://stackoverflow.com/a/61146975/5666845
 
-T_SIZE="$2" # target size in MB
-T_FILE="${1%.*}-$2MB.mp4" # filename out
+TARGET_SIZE="${2:-8}"               # target size in MB, defaults to 8
+#TARGET_SIZE="${2:-100}"             # If you have discord nitro lol
+TARGET_FILE="${1%.*}.${TARGET_SIZE}MB.mp4" # filename out
 
 # Original duration in seconds
-O_DUR=$(\
-    ffprobe \
+ORIGINAL_DURATION=$(
+  ffprobe \
     -v error \
     -show_entries format=duration \
-    -of csv=p=0 "$1")
+    -of csv=p=0 "$1"
+)
 
 # Original audio rate
-O_ARATE=$(\
-    ffprobe \
+ORIGINAL_AUDIO_RATE=$(
+  ffprobe \
     -v error \
     -select_streams a:0 \
     -show_entries stream=bit_rate \
-    -of csv=p=0 "$1")
+    -of csv=p=0 "$1"
+)
 
 # Original audio rate in KiB/s
-O_ARATE=$(\
-    awk \
-    -v arate="$O_ARATE" \
-    'BEGIN { printf "%.0f", (arate / 1024) }')
+ORIGINAL_AUDIO_RATE=$(
+  awk \
+    -v arate="$ORIGINAL_AUDIO_RATE" \
+    'BEGIN { printf "%.0f", (arate / 1024) }'
+)
 
 # Target size is required to be less than the size of the original audio stream
-T_MINSIZE=$(\
-    awk \
-    -v arate="$O_ARATE" \
-    -v duration="$O_DUR" \
-    'BEGIN { printf "%.2f", ( (arate * duration) / 8192 ) }')
+T_MINSIZE=$(
+  awk \
+    -v arate="$ORIGINAL_AUDIO_RATE" \
+    -v duration="$ORIGINAL_DURATION" \
+    'BEGIN { printf "%.2f", ( (arate * duration) / 8192 ) }'
+)
 
 # Equals 1 if target size is ok, 0 otherwise
-IS_MINSIZE=$(\
-    awk \
-    -v size="$T_SIZE" \
+IS_MINSIZE=$(
+  awk \
+    -v size="$TARGET_SIZE" \
     -v minsize="$T_MINSIZE" \
-    'BEGIN { print (minsize < size) }')
+    'BEGIN { print (minsize < size) }'
+)
 
 # Give useful information if size is too small
 if [[ $IS_MINSIZE -eq 0 ]]; then
-    printf "%s\n" "Target size ${T_SIZE}MB is too small!" >&2
-    printf "%s %s\n" "Try values larger than" "${T_MINSIZE}MB" >&2
-    exit 1
+  printf "%s\n" "Target size ${TARGET_SIZE}MB is too small!" >&2
+  printf "%s %s\n" "Try values larger than" "${T_MINSIZE}MB" >&2
+  exit 1
 fi
 
 # Set target audio bitrate
-T_ARATE=$O_ARATE
-
+TARGET_AUDIO_RATE=$ORIGINAL_AUDIO_RATE
 
 # Calculate target video rate - MB -> KiB/s
-T_VRATE=$(\
-    awk \
-    -v size="$T_SIZE" \
-    -v duration="$O_DUR" \
-    -v audio_rate="$O_ARATE" \
-    'BEGIN { print  ( ( size * 8192.0 ) / ( 1.048576 * duration ) - audio_rate) }')
+TARGET_VIDEO_RATE=$(
+  awk \
+    -v size="$TARGET_SIZE" \
+    -v duration="$ORIGINAL_DURATION" \
+    -v audio_rate="$ORIGINAL_AUDIO_RATE" \
+    'BEGIN { print  ( ( size * 8192.0 ) / ( 1.048576 * duration ) - audio_rate) }'
+)
 
 # Perform the conversion
 ffmpeg \
-    -y \
+  -y \
+  -i "$1" \
+  -c:v libx264 \
+  -b:v "$TARGET_VIDEO_RATE"k \
+  -pass 1 \
+  -an \
+  -f mp4 \
+  /dev/null &&
+  ffmpeg \
     -i "$1" \
     -c:v libx264 \
-    -b:v "$T_VRATE"k \
-    -pass 1 \
-    -an \
-    -f mp4 \
-    /dev/null \
-&& \
-ffmpeg \
-    -i "$1" \
-    -c:v libx264 \
-    -b:v "$T_VRATE"k \
+    -b:v "$TARGET_VIDEO_RATE"k \
     -pass 2 \
     -c:a aac \
-    -b:a "$T_ARATE"k \
-    $T_FILE
+    -b:a "$TARGET_AUDIO_RATE"k \
+    "$TARGET_FILE"
